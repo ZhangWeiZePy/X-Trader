ebc8c5297d092f96a4a4d951dd42f1be4fe151ad
zhangweize <zhangweize@eccom.com.cn>
2026-03-26 14:27:27 +0800

    feat: 优化柜台选择逻辑.

--- src/trade-core/interface.cpp
+++ src/trade-core/interface.cpp
@@ -6,13 +6,13 @@
 
 market_api* create_market(std::map<std::string, std::string>& config, std::set<std::string> contracts)
 {
-	if (config["counter"] == "xtp") {
-		return new xtp_market(config, contracts);
-	} else if (config["counter"] == "ctp") {
-		return new ctp_market(config, contracts);
-	} else {
-	    return nullptr;
-	}
+    if (config["counter"] == "xtp") {
+        return new xtp_market(config, contracts);
+    }
+    if (config["counter"] == "ctp") {
+        return new ctp_market(config, contracts);
+    }
+    return nullptr;
 }
 
 void destory_market(market_api*& api)
@@ -25,14 +25,14 @@ void destory_market(market_api*& api)
 
 trader_api* create_trader(std::map<std::string, std::string>& config, std::set<std::string> contracts)
 {
-	if (config["counter"] == "xtp") {
-		return new xtp_trader(config, contracts);
-	}
-    else if (config["counter"] == "ctp") {
-         return new ctp_trader(config, contracts);
-    } else {
-        return nullptr;
+    if (config["counter"] == "xtp") {
+        return new xtp_trader(config, contracts);
     }
+    if (config["counter"] == "ctp") {
+        return new ctp_trader(config, contracts);
+    }
+    return nullptr;
+
 }
 
 void destory_trader(trader_api*& api)
